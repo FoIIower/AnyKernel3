cb5cd29f0c9b0445d5e2027aea53c63d0de415ee
Legend <100516800+FoIIower@users.noreply.github.com>
2022-03-22 11:08:03 +0300

    Shell script to mount cgroups

---
+++ ramdisk/overlay.d/sbin/init.lx.sh
@@ -0,0 +1,10 @@
+#!/system/bin/sh
+mount -t tmpfs -o uid=0,gid=0,mode=0755 cgroup /sys/fs/cgroup
+cd /sys/fs/cgroup/
+mkdir -p cpu cpu acct blkio memory devices pids
+mount -n -t cgroup -o cpu cgroup cpu
+mount -n -t cgroup -o  cpuacct cgroup cpuacct
+mount -n -t cgroup -o  blkio cgroup blkio
+mount -n -t cgroup -o  memory cgroup memory
+mount -n -t cgroup -o  devices cgroup devices
+mount -n -t cgroup -o  pids cgroup pids
